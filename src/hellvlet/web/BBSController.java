eae42c3e1bcfdf7cfa50235e73c7c174c53e235c
JuHyun Lee <leejaycoke@gmail.com>
2016-07-26 23:45:14 +0900

    BBSController 기본 라우팅 추가

--- src/hellvlet/web/BBSController.java
+++ src/hellvlet/web/BBSController.java
@@ -1,8 +1,36 @@
 package hellvlet.web;
 
-import javax.servlet.http.HttpServlet;
+import hellvlet.model.BBS;
 
-public class BBSController extends HttpServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BBSController extends BaseController {
 
 
+    private final List<BBS> mUsers = new ArrayList<>();
+
+    public void bbsListGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        render("/bbs/list.jsp", request, response);
+    }
+
+    public void bbsWriteGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        render("/bbs/write.jsp", request, response);
+    }
+
+    public void bbsEditGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        render("/bbs/edit.jsp", request, response);
+    }
+
+    public void bbsSearchGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        render("/bbs/search.jsp", request, response);
+    }
 }
